03d3b701d66cf67506d8e12c3d70052e45918cbc
prudywsh <antoineprudhomme5@gmail.com>
2017-10-03 22:35:19 +0200

    extended euclide now calculate itself the negative factor in the bezout identity

--- src/lib.py
+++ src/lib.py
@@ -17,12 +17,20 @@ def lcm(a, b):
 def bezout(a, b):
 	""" Calculate the Bézout's identity of 'a' and 'b'
 	"""
-	return euclidean_algorithm(a, b, extended=True)
+	gcd, x, y = euclidean_algorithm(a, b, extended=True)
+
+	# if x and y are reversed, fix it
+	if (a*x + b*y) != gcd:
+		x, y = y, x
+
+	return gcd, x, y
 
 
-def euclidean_algorithm(a, b, x = 0, prev_x = 1, y = 1, prev_y = 0, extended=False):
+def euclidean_algorithm(a, b, x = 0, prev_x = 1, y = 1, prev_y = 0, extended=False, step=1):
 	""" Run the euclidean algorithm to calculate the gcd of a and b
 		If extended is True, calculate x and y for the Bézout's identity
+
+		Step is usefull to know which factor is negative => if even, it's 'y' else it's 'x'
 	"""
 	# 'a' has to be greater than 'b'
 	if b > a:
@@ -33,43 +41,23 @@ def euclidean_algorithm(a, b, x = 0, prev_x = 1, y = 1, prev_y = 0, extended=Fal
 
 	# if remainder is 0, stop here : gcd found
 	if remainder == 0:
-		return (b, x, y) if extended else (b)
+		if not extended:
+			return b
+		else:
+			# use the step parameter to calculate which factor is negative
+			x *= (-1)**step
+			y *= (-1)**(step+1)
+			return b, x, y
 
-	# else, continue
+	# we continue
 	if extended:
-		# if extended, update x and y
+		# if extended, update x and y and increment the step
+		step += 1
 		quotient = a // b
 		prev_x, prev_y, x, y = x, y, quotient*x + prev_x, quotient*y + prev_y
-		return euclidean_algorithm(b, remainder, x, prev_x, y, prev_y, extended=True)
+		return euclidean_algorithm(b, remainder, x, prev_x, y, prev_y, extended=True, step=step)
 	else:
-		return euclidean_algorithm(b, remainder)
-
-
-def find_negative_factor(a, b, gcd, x, y):
-	""" Given 'a' and 'b', and the 2 factors 'x' and 'y'
-		found with the extended euclidean algorithm,
-		search wich factor is negative in the bezout equation.
-
-		we want gcd(a, b) = a * x + b * y
-
-		This function print the result and return new 'x' and 'y'
-	"""
-	if a*x - b*y == gcd:
-		y = -y
-	elif -a*x + b*y == gcd:
-		x = -x
-	elif a*y - b*x == gcd:
-		x, y = y, -x
-	elif -a*y + b*x == gcd:
-		x, y = -y, x
-
-	if y > 0:
-		print("1 = %d x %d + %d x %d" % (x, a, y, b))
-	else:
-		print("1 = %d x %d %d x %d" % (x, a, y, b))
-
-	return x, y
-
+		return euclidean_algorithm(b, remainder, step=step)
 
 def are_coprime(a, b):
 	""" Two integers are coprime if their gcd is 1
--- src/test_bezout.py
+++ src/test_bezout.py
@@ -1,6 +1,6 @@
 import unittest
 
-from lib import bezout, find_negative_factor
+from lib import bezout
 
 class TestBezout(unittest.TestCase):
 
@@ -8,21 +8,18 @@ class TestBezout(unittest.TestCase):
         a = 84
         b = 30
         gcd, x, y = bezout(a, b)
-        x, y = find_negative_factor(a, b, gcd, x, y)
         self.assertEqual(x*a + y*b, gcd)
 
     def test_with_prime(self):
         a = 17
         b = 9
         gcd, x, y = bezout(a, b)
-        x, y = find_negative_factor(a, b, gcd, x, y)
         self.assertEqual(x*a + y*b, 1)
 
     def test_with_prime_inverse(self):
         a = 9
         b = 17
         gcd, x, y = bezout(a, b)
-        x, y = find_negative_factor(a, b, gcd, x, y)
         self.assertEqual(x*a + y*b, 1)
 
 if __name__ == '__main__':
