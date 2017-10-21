f6ff22744582667d0cdbf8e4cac7533e3c7f6e34
prudywsh <antoineprudhomme5@gmail.com>
2017-10-21 16:01:53 +0200

    binary to unicode function

--- src/test_text_to_binary.py
+++ src/test_text_to_binary.py
@@ -1,14 +1,18 @@
 import unittest
 
-from utils import utf8_to_binary
+from utils import utf8_to_binary, binary_to_utf8
 
 class TestTextToBinary(unittest.TestCase):
 
-    def test_simple_case(self):
+    def test_to_binary_simple_case(self):
         self.assertEqual(utf8_to_binary('hello world'), '0110100001100101011011000110110001101111001000000111011101101111011100100110110001100100')
 
-    def test_with_accents(self):
+    def test_to_binary_with_accents(self):
         self.assertEqual(utf8_to_binary("J'ai été à la pèche."), '0100101000100111011000010110100100100000111010010111010011101001001000001110000000100000011011000110000100100000011100001110100001100011011010000110010100101110')
+
+    def test_utf8_binary_utf8(self):
+        text = "J'ai été à la pèche."
+        self.assertEqual(binary_to_utf8(utf8_to_binary(text)), text)
 
 if __name__ == '__main__':
     unittest.main()
--- src/utils.py
+++ src/utils.py
@@ -14,3 +14,12 @@ def utf8_to_binary(text):
     """ Convert unicode (utf-8) text to binary
     """
     return ''.join("{0:08b}".format(ord(c)) for c in text)
+
+def binary_to_utf8(b):
+    """ Convert binary text to uncode text (utf-8)
+    """
+    block_size = 8
+    # start by cutting the text in blocks of 8 bits
+    # then, convert each block from binary to decimal and from decimal to char
+    # join the results
+    return ''.join([chr(int(c, 2)) for c in list(b[i:i+block_size] for i in range(0, len(b), block_size))])
