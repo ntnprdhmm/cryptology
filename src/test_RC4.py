3bcf09882fe729f0ee8a1ec977102ab3eba91fee
prudywsh <antoineprudhomme5@gmail.com>
2017-10-21 22:28:50 +0200

    RC4 decode test added

--- src/test_RC4.py
+++ src/test_RC4.py
@@ -14,5 +14,10 @@ class TestGCD(unittest.TestCase):
         key = "Key"
         self.assertEqual(rc4(text, key), "»ó\x16èÙ@¯\nÓ")
 
+    def test_rc4_decrypt(self):
+        text = "Hello world 45 ! azeaze è&"
+        key = "ALittleKey:3"
+        self.assertEqual(rc4(rc4(text, key), key), text)
+
 if __name__ == '__main__':
     unittest.main()
