e7949f632631b619b8094aa6166585f09b8e1330
prudywsh <antoineprudhomme5@gmail.com>
2017-10-31 21:22:53 +0100

    A5/1: generate sequences

--- src/A51.py
+++ src/A51.py
@@ -32,7 +32,34 @@ class A51:
     def output_bit(self):
         """ XOR the ouput bits of the 3 registers
         """
-        return r1.output_bit() ^ r2.output_bit() ^ r3.output_bit()
+        return self.r1.output_bit() ^ self.r2.output_bit() ^ self.r3.output_bit()
+
+    def gen_sequence(self):
+        """ Generate a sequence of 114 bits
+        """
+        sequence = []
+        for i in range(114):
+            # check the clocking bit of each register
+            # to determine the majority bit
+            c = {0:0, 1:0}
+            c[self.r1.clocking_bit()] += 1
+            c[self.r2.clocking_bit()] += 1
+            c[self.r3.clocking_bit()] += 1
+
+            majority_bit = 0 if c[0] > c[1] else 1
+
+            # update LFSR where clocking_bit = majority_bit
+            if self.r1.clocking_bit() == majority_bit:
+                self.r1.shift()
+            if self.r2.clocking_bit() == majority_bit:
+                self.r2.shift()
+            if self.r3.clocking_bit() == majority_bit:
+                self.r3.shift()
+
+            sequence.append(self.output_bit())
+
+        return ''.join([str(b) for b in sequence])
 
 alg = A51()
 alg.init_lfsr("1111000011110000111100001111000011110000111100001111000011110000", "1100110011001100110001")
+print(alg.gen_sequence())
