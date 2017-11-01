93e9f26b1ad805b42567109504513c676e0eb156
prudywsh <antoineprudhomme5@gmail.com>
2017-11-01 21:09:55 +0100

    DES: started to write feistel rounds + subkeys generator done

--- src/DES.py
+++ src/DES.py
@@ -1,11 +1,15 @@
 # initial permutation table
-IPtable = (57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7, 56, 48, 40, 32, 24, 16, 8, 0, 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6)
-# expension table
-expansion_table = (31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8, 7, 8, 9, 10, 11, 12, 11, 12, 13, 14, 15, 16, 15, 16, 17, 18, 19, 20, 19, 20, 21, 22, 23, 24, 23, 24, 25, 26, 27, 28, 27, 28, 29, 30, 31, 0)
-
-PFtable = (15, 6, 19, 20, 28, 11, 27, 16, 0, 14, 22, 25, 4, 17, 30, 9, 1, 7, 23, 13, 31, 26, 2, 8, 18, 12, 29, 5, 21, 10, 3, 24)
-
-FPtable = (39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25, 32, 0, 40, 8, 48, 16, 56, 24)
+IP = (57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7, 56, 48, 40, 32, 24, 16, 8, 0, 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6)
+# expension
+E = (31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8, 7, 8, 9, 10, 11, 12, 11, 12, 13, 14, 15, 16, 15, 16, 17, 18, 19, 20, 19, 20, 21, 22, 23, 24, 23, 24, 25, 26, 27, 28, 27, 28, 29, 30, 31, 0)
+# permutation
+P = (15, 6, 19, 20, 28, 11, 27, 16, 0, 14, 22, 25, 4, 17, 30, 9, 1, 7, 23, 13, 31, 26, 2, 8, 18, 12, 29, 5, 21, 10, 3, 24)
+# final permutation
+IPinv = (39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25, 32, 0, 40, 8, 48, 16, 56, 24)
+# permuted choice 1
+PC1 = (57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4)
+# Permuted choice 2
+PC2 = (13, 16, 10, 23, 0, 4, 2, 27, 14, 5, 20, 9, 22, 18, 11, 3, 25, 7, 15, 6, 26, 19, 12, 1, 40, 51, 30, 36, 46, 54, 29, 39, 50, 44, 32, 47, 43, 48, 38, 55, 33, 52, 45, 41, 49, 35, 28, 31)
 
 sBox = 8*[64*[0]]
 
@@ -52,15 +56,34 @@ import utils
 
 class DES:
 
-    def __init__(self):
-        pass
+    def __init__(self, key):
+        self.rounds = 16
+        self.key = key
+        self.generate_subkeys()
+
+    def generate_subkeys(self):
+        # 64-bits key to 56-bits permuted key
+        self.permuted_key = ''.join([self.key[i] for i in PC1])
+        # split the 56-bits in 2 blocks
+        pk1 = self.permuted_key[:26]
+        pk2 = self.permuted_key[26:]
+        # generate the 16 subkeys
+        self.subkeys = []
+        for _ in range(16):
+            # rotate by one to the left each block
+            pk1 = pk1[1:] + pk1[0]
+            pk2 = pk2[1:] + pk2[0]
+            # generate the next subkey
+            key = pk1 + pk2
+            self.subkeys.append(''.join([key[i] for i in PC2]))
+        print(self.subkeys)
 
     def expansion(self, m):
         """ Step 1:
             the 32-bit half-block is expanded to 48 bits
             using the expansion permutation
         """
-        return ''.join([m[i] for i in expansion_table])
+        return ''.join([m[i] for i in E])
 
     def key_mixing(self, k, m):
         """ Step 2:
@@ -88,7 +111,7 @@ class DES:
             the 32 output bits from the S-boxes are rearranged
             according to a fixed permutation,
         """
-        return ''.join([c[i] for i in PFtable])
+        return ''.join([c[i] for i in P])
 
     def F(self, k, m):
         """ Feistel function for DES
@@ -100,6 +123,22 @@ class DES:
 
         return permuted
 
+    def cipher(self, m):
+        # initial permutation
+        m = [m[i] for i in IP]
+        # divide in 2 block of 32 bits
+        G = M[:(len(M)//2)]
+        D = M[len(M)//2:]
 
-des = DES()
-des.F("110011001100110011001100110011001100110011001100", "11111111000000001111111100000000")
+        # 16 feistel rounds
+        for _ in range(self.rounds):
+            pass
+
+        # final permutation
+        m = [m[i] for i in IPinv]
+
+        return m
+
+
+des = DES("1100110011001100110011001100110011001100110011001100110011001100")
+#des.F("11111111000000001111111100000000")
