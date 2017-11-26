f23b213142f318c38c150e2b0488a8fef6d3288a
prudywsh <antoineprudhomme5@gmail.com>
2017-11-26 22:07:29 +0100

    CramerShoup: started to do the key generation

---
+++ src/CramerShoup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" This module contains the CramerShoup class
+"""
+
+from random import randint
+
+class CramerShoup(object):
+    """ CramerShoup implementation
+    """
+    def __init__(self):
+        self.p = 67
+        self.a1 = 34
+        self.a2 = 41
+        self.X = 0
+        self.Y = 0
+        self.W = 0
+
+    def key_generation(self):
+        """ Generate the public key (p, a1, a2, X, Y, W)
+        """
+        # pick randomly 5 integers in range [0:P]
+        x1 = randint(0, self.p)
+        x2 = randint(0, self.p)
+        y1 = randint(0, self.p)
+        y2 = randint(0, self.p)
+        w = randint(0, self.p)
+        # calculate X, Y and W
+        self.X = self.a1**x1 + self.a2**x2
+        self.Y = self.a1**y1 + self.a2**y2
+        self.W = self.a1**w
+
+c = CramerShoup()
+c.key_generation()
+print(c.X)
+print(c.Y)
+print(c.W)
