4b8b5db8cada63e8fee1ab023b84c3ffeb90f1bd
prudywsh <antoineprudhomme5@gmail.com>
2017-11-28 18:29:48 +0100

    SHA1: refactoring but still wrong. Commit before try smth

--- src/SHA1.py
+++ src/SHA1.py
@@ -16,116 +16,121 @@ class SHA1(object):
     """
 
     def __init__(self):
-        self.block_size = 64
-        self.h = [0x67452301,
-                  0xEFCDAB89,
-                  0x98BADCFE,
-                  0x10325476,
-                  0xC3D2E1F0]
+        self.mask = 0xffffffff
+        self._H = [0x67452301,
+                   0xEFCDAB89,
+                   0x98BADCFE,
+                   0x10325476,
+                   0xC3D2E1F0]
 
-    def produce_digest(self):
-        """ Combine the 5 hash variables to produce the final hash
-            return the 160 bits length hash
+    @staticmethod
+    def _padding(stream):
         """
-        # we return 5 blocks of 8 hexadecimal digits
-        return '%08x%08x%08x%08x%08x' % (self.h[0] & 0xffffffff,
-                                         self.h[1] & 0xffffffff,
-                                         self.h[2] & 0xffffffff,
-                                         self.h[3] & 0xffffffff,
-                                         self.h[4] & 0xffffffff)
-
-    def pad(self, arr):
-        """
-            SHA-1 works with blocks of 64 bytes (512 bits). If the number of bits
-            in the text is not a multiple of 512, add some padding:
+            If the number of bits is not a multiple of 512, add some padding:
                 - add '1' at the end of the text
                 - fill with '0' (but let 64 bits available at the end)
                 - the 64 last bits are the length of the original text
 
             Args:
-                arr -- bytearray -- the text to hash in bytes
+                stream -- bytearray -- the stream to hash
 
-            return arr, padded if needed
+            return stream, padded if needed
         """
-        original_length = len(arr)
+        original_length = len(stream)
 
-        if len(arr) % self.block_size == 0:
-            return arr
+        if len(stream) % 64 == 0:
+            return stream
 
         # append the bit 1
-        arr.append(0x80)
+        stream.append(0x80)
 
-        # add k*'0', with len(arr) + k = 56 (mod 64)
+        # add k*'0', with len(stream) + k = 56 (mod 64)
         # => to let 8 bytes (64 bits) for original text length
-        nb_zero_to_add = ((7*self.block_size)//8 - len(arr)) % self.block_size
+        nb_zero_to_add = (56 - len(stream)) % 64
         for _ in range(nb_zero_to_add):
-            arr.append(0)
+            stream.append(0)
 
         # add the length of the original text
-        arr += struct.pack(b'>Q', original_length)
+        stream += struct.pack(b'>Q', original_length)
 
-        return arr
+        return stream
 
-    def hash(self, text):
+    @staticmethod
+    def _prepare(stream):
         """
-            Hash the given text
+            Cut the stream in blocks of 64 bytes, and each block in 16 words
+            of 4 bytes
 
             Args:
-                text -- string -- the text to hash
+                stream -- bytearray -- the stream to hash
+
+            return a list of blocks
+        """
+        blocks = []
+        nb_blocks = len(stream) // 64
+
+        for i in range(nb_blocks):
+            temp = []
+            for j in range(16):
+                # Calculate the value of the word and append it
+                value = 0
+                for k in range(4):
+                    value <<= 8
+                    value += stream[i*64 + j*4 + k]
+                temp.append(value)
+            blocks.append(temp)
+
+        return blocks
+
+    def hash(self, stream):
+        """
+            Hash the given stream
+
+            Args:
+                stream -- string -- the text to hash
 
             return the 40 bytes digest
         """
-        # transform the string to an array of bytes
-        bytes_text = bytearray(text, 'utf-8')
-        # SHA-1 works with 512 bits blocks.
-        # add some padding if needed
-        bytes_text = self.pad(bytes_text)
+        # transform the string to an array of bytes and prepare it
+        stream = self._prepare(self._padding(bytearray(stream, 'utf-8')))
 
-        # process the text in blocks of 64 bits
-        for i in range(0, len(bytes_text), self.block_size):
-            self.process_block(bytes_text[i:i+self.block_size])
+        for block in stream:
+            self._process_block(block)
 
-        # return the digest
         return self.produce_digest()
 
-    def process_block(self, block):
+    def _process_block(self, block):
         """
-            Hash the current block and update the hash variable
+            Process the current block and update the hash variable
 
             Args:
                 block -- bytearray -- the block to hash
         """
-        w = [0]*80
-        # cut the block in 16 words of 4 bytes
-        for t in range(16):
-            w[t] = struct.unpack(b'>I', block[t*4:(t+1)*4])[0]
-        # => extend it to 80 words
-        for t in range(16, 80):
-            w[t] = rotl((w[t-3] ^ w[t-8] ^ w[t-14] ^ w[t-16]))
+        # => extend the block from 16 to 80 words
+        w = block[:]
+        for i in range(16, 80):
+            w.append(rotl((w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16])))
 
         # initialize hash value for this block
-        a = self.h[0]
-        b = self.h[1]
-        c = self.h[2]
-        d = self.h[3]
-        e = self.h[4]
+        a, b, c, d, e = self._H[:]
 
         # main loop
         for i in range(80):
             if i <= 19:
-                f = (b and c) or ((not b) and d)
+                #f = (b and c) or ((not b) and d)
+                f = (b & c) ^ (~b & d)
                 k = 0x5A827999
             elif i <= 39:
                 f = b ^ c ^ d
                 k = 0x6ED9EBA1
             elif i <= 59:
-                f = (b and c) or (b and d) or (c and d)
+                f = (b & c) ^ (b & d) ^ (c & d)
                 k = 0x8F1BBCDC
             else:
                 f = b ^ c ^ d
                 k = 0xCA62C1D6
 
-            T = (rotl(a, 5) + f + e + k + w[i]) & 0xffffffff
+            T = (rotl(a, 5) + f + e + k + w[i]) & self.mask
             e = d
             d = c
             c = rotl(b, 30)
@@ -133,8 +138,16 @@ class SHA1(object):
             a = T
 
         # add the block hash to the result
-        self.h[0] += (self.h[0] + a) & 0xffffffff
-        self.h[1] += (self.h[1] + b) & 0xffffffff
-        self.h[2] += (self.h[2] + c) & 0xffffffff
-        self.h[3] += (self.h[3] + d) & 0xffffffff
-        self.h[4] += (self.h[4] + e) & 0xffffffff
+        self._H[0] += (self._H[0] + a) & self.mask
+        self._H[1] += (self._H[1] + b) & self.mask
+        self._H[2] += (self._H[2] + c) & self.mask
+        self._H[3] += (self._H[3] + d) & self.mask
+        self._H[4] += (self._H[4] + e) & self.mask
+
+    def produce_digest(self):
+        """ Combine the 5 hash variables to produce the final hash
+
+            return the 160 bits length hash
+        """
+        # return 5 blocks of 8 hexadecimal digits
+        return ''.join([('%08x' % h) for h in self._H])
--- tests/test_SHA1.py
+++ tests/test_SHA1.py
@@ -2,19 +2,30 @@ import unittest
 
 from src.SHA1 import SHA1
 
-class TestGCD(unittest.TestCase):
+class TestSHA1(unittest.TestCase):
 
-    def test_simple_1(self):
+    def test_padding_simple_1(self):
         sha_1 = SHA1()
         text = "Hello world !"
         bytetext = bytearray(text, 'utf-8')
-        self.assertEqual(len(sha_1.pad(bytetext)) % sha_1.block_size, 0)
+        self.assertEqual(len(SHA1._padding(bytetext)) % sha_1.block_size, 0)
 
-    def test_simple_2(self):
+    def test_padding_simple_2(self):
         sha_1 = SHA1()
         text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         bytetext = bytearray(text, 'utf-8')
-        self.assertEqual(len(sha_1.pad(bytetext)) % sha_1.block_size, 0)
+        self.assertEqual(len(SHA1._padding(bytetext)) % sha_1.block_size, 0)
+
+    def test_prepare_simple(self):
+        sha_1 = SHA1()
+        text = "Hello world !"
+        bytetext = bytearray(text, 'utf-8')
+
+        # value of b'Hell'
+        result = 1214606444
+
+        # test 'Hell', the first word of the first block
+        self.assertEqual(SHA1._prepare(SHA1._padding(bytetext))[0][0], result)
 
 if __name__ == '__main__':
     unittest.main()
